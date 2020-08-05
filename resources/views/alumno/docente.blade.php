43e587c180001dccdb3e86574bdf64fa748b96a5
Juan Ignacio <jcorderuiz@gmail.com>
2020-08-05 17:56:08 -0400

    refactor hrefs

--- resources/views/alumno/docente.blade.php
+++ resources/views/alumno/docente.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="content">
 <div id="content-header">
-  <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#" class="tip-bottom">Alumno</a> </div>
+  <div id="breadcrumb"> <a href="{{ route('dashboard.index') }}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#" class="tip-bottom">Alumno</a> </div>
   <h1>Asignar alumnos y retroalimentaciones</h1>
 </div>
 
--- resources/views/alumno/retroalimentaciones.blade.php
+++ resources/views/alumno/retroalimentaciones.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="content">
 <div id="content-header">
-  <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="{{ route('alumno.index') }}" class="tip-bottom">Alumno</a> <a href="#" class="current">Retroalimentaciones</a> </div>
+  <div id="breadcrumb"> <a href="{{ route('dashboard.index') }}" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="{{ route('alumno.index') }}" class="tip-bottom">Alumno</a> <a href="#" class="current">Retroalimentaciones</a> </div>
   <h1>Clases y retroalimentaciones</h1>
 </div>
 
