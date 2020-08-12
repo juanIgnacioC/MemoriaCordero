dae4d549ffc3e3f333c6d6aa2a68a27b8b792de6
Juan Ignacio <jcorderuiz@gmail.com>
2020-08-11 23:35:54 -0400

    register fix layout?

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,6 @@
-{{-- @extends('layouts.app') --}}
+@extends('layouts.register')
 
-
-{{-- @section('content') --}}
-
-
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -77,4 +74,4 @@
         </div>
     </div>
 </div>
-{{-- @endsection --}}
+@endsection
